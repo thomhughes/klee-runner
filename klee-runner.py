9e598ff4fb414605b902bc30708ddd67f5243910
Dan Liew <daniel.liew@imperial.ac.uk>
2016-09-28 13:48:13 +0100

    Change `klee-runner.py` to not need an invocation info file. Instead the invocation info is specified on the command line which is way more convenient and is great for quick testing. However this makes `klee-runner.py` much more specific (it's only useful for running KLEE for exploration and not for replay).
    I'm not sure if this is the right trade-off. The batch runner will
    work with invocation info  files though so at least tool (when I write
    it) will be general purpose.

--- klee-runner.py
+++ klee-runner.py
@@ -9,6 +9,7 @@ import os
 from  KleeRunner import InvocationInfo
 from KleeRunner import ConfigLoader
 from KleeRunner import RunnerFactory
+import pprint
 import traceback
 import yaml
 import sys
@@ -18,11 +19,31 @@ def entryPoint(args):
   parser.add_argument("-l","--log-level",type=str, default="info", dest="log_level", choices=['debug','info','warning','error'])
   parser.add_argument("--dry", action='store_true', help="Stop after initialising runners")
   parser.add_argument("config_file", help="YAML configuration file")
-  parser.add_argument("invocation_info", help="invocation info")
+  # parser.add_argument("invocation_info", help="invocation info")
   parser.add_argument("working_dir", help="Working directory")
   parser.add_argument("yaml_output", help="path to write YAML output to")
+  parser.add_argument("program", help="Program to run")
+  # `program_args` is a dummy argument so we get the right usage message from argparse.
+  # We parse `programs_args` ourselves
+  parser.add_argument("program_args", nargs="*", help="Arguments to pass to program")
 
-  pargs = parser.parse_args()
+  # Determine where the `program` argument is so
+  # we split the arguments into to lists. One to give
+  # to argparse and the other to use in an instance of InvocationInfo.
+  # FIXME: We need a better heuristic.
+  index = 0
+  while index < (len(args) -1):
+    # FIXME: We need to support native executables too.
+    if args[index].endswith('.bc'): # This is such a hack!
+      break
+    index += 1
+
+  argParseArgs = args[:index+1]
+  programArgs = []
+  if len(args) >= index + 2:
+    programArgs = args[index+1:]
+
+  pargs = parser.parse_args(argParseArgs)
 
   logLevel = getattr(logging, pargs.log_level.upper(),None)
   if logLevel == logging.DEBUG:
@@ -48,20 +69,15 @@ def entryPoint(args):
     _logger.debug(traceback.format_exc())
     return 1
 
-  # Load invocation info
-  invocationInfo = None
-  try:
-    with open(pargs.invocation_info, 'r') as f:
-      invocationInfo = InvocationInfo.loadInvocationInfo(f)
-  except InvocationInfo.InvocationInfoValidationError as e:
-    _logger.error('Failed to validate invocation info')
-    _logger.error(e)
-    _logger.debug(traceback.format_exc())
-    return 1
-  except Exception as e:
-    _logger.error(e)
-    _logger.debug(traceback.format_exc())
-    return 1
+  # Create invocation info
+  invocationInfoRepr = {
+    'program': pargs.program,
+    'command_line_arguments': programArgs,
+    'environment_variables': {},
+    'extra_klee_arguments': [],
+  }
+  print(invocationInfoRepr)
+  invocationInfo = InvocationInfo.InvocationInfo(invocationInfoRepr)
 
   # Setup the working directory
   workDir = os.path.abspath(pargs.working_dir)
@@ -87,6 +103,11 @@ def entryPoint(args):
       return 1
 
   # Get Runner class to use
+  # FIXME: Not sure how we want this tool to work with multiple use cases
+  # (i.e. running natively with ktest files, or running klee in replay
+  # mode with ktest files). For now just force this particular running
+  # as a sanity check.
+  assert config['runner'] == 'Klee'
   RunnerClass = RunnerFactory.getRunnerClass(config['runner'])
   runner = RunnerClass(invocationInfo, workDir, config['runner_config'])
 
