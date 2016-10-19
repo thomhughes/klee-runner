bf9f9efbfa2ccb881ec3161bd9ef406aa22cc4f3
Dan Liew <daniel.liew@imperial.ac.uk>
2016-10-19 15:32:23 +0100

    Add a schema for ResultInfo and add a tool to filter results based on an arbitary predicate.

---
+++ KleeRunner/ResultInfo.py
@@ -0,0 +1,138 @@
+# Copyright (c) 2016, Daniel Liew
+# This file is covered by the license in LICENSE
+from . import util
+import collections
+import copy
+import os
+import pprint
+import yaml
+import jsonschema
+
+class ResultInfo:
+  def __init__(self, data):
+    assert isinstance(data, dict)
+    self._data = data
+
+# TODO: Implement property getters
+
+  def GetInternalRepr(self):
+    return self._data
+
+class ResultInfoValidationError(Exception):
+  def __init__(self, message, absoluteSchemaPath=None):
+    assert isinstance(message, str)
+    if absoluteSchemaPath != None:
+      assert isinstance(absoluteSchemaPath, collections.deque)
+    self.message = message
+    self.absoluteSchemaPath = absoluteSchemaPath
+
+  def __str__(self):
+    return self.message
+
+def loadResultInfos(openFile):
+  resultInfos = loadRawResultInfos(openFile)
+  resultInfoObjects = []
+  for r in resultInfos['results']:
+    resultInfoObjects.append(ResultInfo(r))
+  return resultInfoObjects
+
+def loadRawResultInfos(openFile):
+  resultInfos = util.loadYaml(openFile)
+  validateResultInfos(resultInfos)
+  return resultInfos
+
+def getSchema():
+  """
+    Return the Schema for ResultInfo files.
+  """
+  yamlFile = os.path.join(os.path.dirname(__file__), 'ResultInfoSchema.yml')
+  schema = None
+  with open(yamlFile, 'r') as f:
+    schema = util.loadYaml(f)
+  assert isinstance(schema, dict)
+  assert '__version__' in schema
+  return schema
+
+
+def validateResultInfos(resultInfos, schema=None):
+  """
+    Validate a ``resultInfo`` file.
+    Will throw a ``ResultInfoValidationError`` exception if
+    something is wrong
+  """
+  assert isinstance(resultInfos, dict)
+  if schema == None:
+    schema = getSchema()
+  assert isinstance(schema, dict)
+  assert '__version__' in schema
+
+  # Even though the schema validates this field in the resultInfo we need to
+  # check them ourselves first because if the schema version we have doesn't
+  # match then we can't validate using it.
+  if 'schema_version' not in resultInfos:
+    raise ResultInfoValidationError(
+      "'schema_version' is missing")
+  if not isinstance(resultInfos['schema_version'], int):
+    raise ResultInfoValidationError(
+      "'schema_version' should map to an integer")
+  if not resultInfos['schema_version'] >= 0:
+    raise ResultInfoValidationError(
+      "'schema_version' should map to an integer >= 0")
+  if resultInfos['schema_version'] != schema['__version__']:
+    raise ResultInfoValidationError(
+        ('Schema version used by benchmark ({}) does not match' +
+        ' the currently support schema ({})').format(
+          resultInfos['schema_version'],
+          schema['__version__']))
+
+  # Validate against the schema
+  try:
+    jsonschema.validate(resultInfos, schema)
+  except jsonschema.exceptions.ValidationError as e:
+    raise ResultInfoValidationError(
+        str(e),
+        e.absolute_schema_path)
+  return
+
+def upgradeResultInfosToVersion(resultInfos, schemaVersion):
+  """
+    Upgrade invocation info to a particular schemaVersion. This
+    does not validate it against the schema.
+  """
+  assert isinstance(resultInfos, dict)
+  assert isinstance(schemaVersion, int)
+  schemaVersionUsedByInstance = resultInfos['schema_version']
+  assert isinstance(schemaVersionUsedByInstance, int)
+  assert schemaVersionUsedByInstance >= 0
+  assert schemaVersion >= 0
+  newResultInfo = copy.deepcopy(resultInfos)
+
+  if schemaVersionUsedByInstance == schemaVersion:
+    # Nothing todo
+    return newResultInfo
+  elif schemaVersionUsedByInstance > schemaVersion:
+    raise Exception('Cannot downgrade benchmark specification to older schema')
+
+  # TODO: Implement upgrade if we introduce new schema versions
+  # We would implement various upgrade functions (e.g. ``upgrade_0_to_1()``, ``upgrade_1_to_2()``)
+  # and call them successively until the ``resultInfos`` has been upgraded to the correct version.
+  raise NotImplementedException()
+
+def upgradeResultInfosToSchema(resultInfos, schema=None):
+  """
+    Upgrade a ``invocationInfo`` to the specified ``schema``.
+    The resulting ``invocationInfo`` is validated against that schema.
+  """
+  if schema == None:
+    schema = getSchema()
+  assert '__version__' in schema
+  assert 'schema_version' in resultInfos
+
+  newResultInfos = upgradeResultInfosToVersion(
+      resultInfos,
+      schema['__version__']
+  )
+
+  # Check the upgraded resultInfos against the schema
+  validateResultInfos(newResultInfos, schema=schema)
+  return newResultInfos
---
+++ tools/result-info-filter.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Copyright (c) 2016, Daniel Liew
+# This file is covered by the license in LICENSE-SVCB.txt
+"""
+Read a result info files and filter based on a predicate
+"""
+from load_klee_runner import add_KleeRunner_to_module_search_path
+add_KleeRunner_to_module_search_path()
+from KleeRunner import ResultInfo
+
+import argparse
+import logging
+import os
+import pprint
+import re
+import sys
+import yaml
+
+_logger = None
+
+def main(args):
+  global _logger
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument("-l","--log-level",type=str, default="info",
+                      dest="log_level",
+                      choices=['debug','info','warning','error'])
+  parser.add_argument('result_info_file',
+                      help='Invocation info file',
+                      type=argparse.FileType('r'))
+  parser.add_argument('predicate',
+                      type=str,
+                      help="python expression to evaluate on result 'r'")
+  parser.add_argument('-o', '--output',
+                      type=argparse.FileType('w'),
+                      default=sys.stdout,
+                      help='Output location (default stdout)')
+
+  pargs = parser.parse_args()
+  logLevel = getattr(logging, pargs.log_level.upper(),None)
+  logging.basicConfig(level=logLevel)
+  _logger = logging.getLogger(__name__)
+
+  resultInfos = ResultInfo.loadRawResultInfos(pargs.result_info_file)
+  # Make shallow copy
+  newResultInfos = resultInfos.copy()
+  newResultInfos['results'] = [ ]
+
+  # FIXME: Should we try sanity check the predicate? The user
+  # could specify literaly anything and could be dangerous to
+  # execute.
+  _logger.info('Compiling predicate "{}"'.format(pargs.predicate))
+  predicate = eval('lambda r: {}'.format(pargs.predicate))
+
+  # filter out non matching jobs by only copying over results that
+  # match the predicate
+  keepCount = 0
+  for r in resultInfos['results']:
+    if predicate(r):
+      _logger.debug('Keeping result "{}"'.format(r))
+      newResultInfos['results'].append(r)
+      keepCount += 1
+    else:
+      _logger.debug('Removing result "{}"'.format(r))
+
+  # Output as YAML
+  pargs.output.write('# Automatically generated result info\n')
+  pargs.output.write(yaml.dump(newResultInfos, default_flow_style=False))
+
+  _logger.info('# kept: {}'.format(keepCount))
+  _logger.info('# removed: {}'.format(len(resultInfos['results']) - keepCount))
+
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
