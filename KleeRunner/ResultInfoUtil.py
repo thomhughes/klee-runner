04684b820b9b4d84594b5776377412b919fa9997
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-16 12:41:22 +0000

    Start implementing result info ranking code.

---
+++ KleeRunner/ResultInfoUtil.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# vim: set sw=4 ts=4 softtabstop=4 expandtab:
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def get_result_info_key(ri):
+    return ri['invocation_info']['program']
+
+def group_result_infos_by(result_infos_list, key_fn=get_result_info_key):
+    """
+    Given a list of raw `ResultInfos` group them by `key_fn`. `key_fn` should be a
+    function that takes a raw `ResultInfo` and returns a unique identifier.
+
+    Returns a tuple (key_to_result_infos, rejected_result_infos)
+
+    `key_to_result_infos` is a dictionary mapping the key to a list of raw `ResultInfo`s
+    where the index of the `ResultInfo` corresponds to the index in which it was found
+    in `result_infos_list`.
+
+    `rejected_result_infos` is a dictionary containing rejected raw `ResultInfo`s.
+    It maps the index of the raw `ResultInfos` (in `result_infos_list`) to
+    a list of rejected raw `ResultInfo`s.
+    """
+    rejected_result_infos = dict()
+    assert(len(result_infos_list) > 1)
+
+    key_to_result_infos = dict()
+    number_of_result_infos = len(result_infos_list)
+
+    defaultGroup = []
+    for index in range(0, number_of_result_infos):
+        defaultGroup.append(None)
+        rejected_result_infos[index] = []
+    index = None
+    assert(len(defaultGroup) == number_of_result_infos)
+    assert(len(rejected_result_infos) == number_of_result_infos)
+
+    for result_infos_index in range(0, number_of_result_infos):
+        for r in result_infos_list[result_infos_index]['results']:
+            key = key_fn(r)
+            if key not in key_to_result_infos:
+                key_to_result_infos[key] = defaultGroup.copy()
+                _logger.debug('Added key "{}" = {}'.format(key, key_to_result_infos[key]))
+            group_for_key = key_to_result_infos[key]
+            if group_for_key[result_infos_index] is not None:
+                _logger.error(
+                    '"{}" cannot appear more than once in the same result infos (index {})'.format(
+                    key, result_infos_index))
+                _logger.debug('group_for_key: {}'.format(group_for_key))
+                rejected_result_infos[result_infos_index].append(r)
+                continue
+            else:
+                _logger.debug('Inserting at index {} for key "{}"'.format(result_infos_index, key))
+                key_to_result_infos[key][result_infos_index] = r
+
+    return (key_to_result_infos, rejected_result_infos)
---
+++ tools/result-info-rank.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# vim: set sw=4 ts=4 softtabstop=4 expandtab:
+"""
+Take two klee-runner output files and rank their results
+in terms of bug finding.
+"""
+
+import argparse
+import logging
+import pprint
+import sys
+from load_klee_analysis import add_kleeanalysis_to_module_search_path
+from load_klee_runner import add_KleeRunner_to_module_search_path
+add_kleeanalysis_to_module_search_path()
+import KleeRunner.ResultInfo
+import KleeRunner.DriverUtil as DriverUtil
+import KleeRunner.ResultInfoUtil
+
+_logger = logging.getLogger(__name__)
+
+def handle_rejected_result_infos(rejected_result_infos, index_to_name_fn):
+    assert(len(rejected_result_infos) == 2)
+    had_rejected_result_infos = False
+    for index, rejected_result_infos_list in rejected_result_infos.items():
+        name = index_to_name_fn(index)
+        assert(isinstance(rejected_result_infos_list, list))
+        for result_info in rejected_result_infos_list:
+            had_rejected_result_infos = True
+            _logger.warning('"{}" was rejected from "{}"'.format(
+                KleeRunner.ResultInfoUtil.get_result_info_key(result_info),
+                name))
+    return had_rejected_result_infos
+
+def report_missing_result_infos(key_to_result_infos, index_to_name_fn):
+    had_missing_result_infos = False
+    for key, result_infos in key_to_result_infos.items():
+        assert(isinstance(result_infos, list))
+        for index, result_info in enumerate(result_infos):
+            if result_info is None:
+                had_missing_result_infos = True
+                name = index_to_name_fn(index)
+                _logger.warning('"{}" is missing from "{}"'.format(
+                    key,
+                    name))
+    return had_missing_result_infos
+
+def main(argv):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("first_result_info_file",
+                        help="First result info fle",
+                        type=argparse.FileType('r'))
+    parser.add_argument("second_result_info_file",
+                        help="Second result info fle",
+                        type=argparse.FileType('r'))
+    DriverUtil.parserAddLoggerArg(parser)
+
+    args = parser.parse_args(args=argv)
+    DriverUtil.handleLoggerArgs(args)
+
+    key_to_result_infos = None
+    rejected_result_infos = None
+    try:
+        # FIXME: Don't use raw form
+        if args.first_result_info_file.name == args.second_result_info_file.name:
+            _logger.error("First and second result-infos file cannot be the same")
+            return 1
+
+        _logger.info('Loading "{}"'.format(args.first_result_info_file.name))
+        firstResultInfos = KleeRunner.ResultInfo.loadRawResultInfos(
+            args.first_result_info_file)
+        _logger.info('Loading "{}"'.format(args.second_result_info_file.name))
+        secondResultInfos = KleeRunner.ResultInfo.loadRawResultInfos(
+            args.second_result_info_file)
+
+        result_infos_list = [ firstResultInfos, secondResultInfos ]
+        key_to_result_infos, rejected_result_infos = (
+            KleeRunner.ResultInfoUtil.group_result_infos_by(result_infos_list)
+        )
+        def index_to_name_fn(index):
+            if index == 0:
+                return args.first_result_info_file.name
+            elif index == 1:
+                return args.second_result_info_file.name
+            else:
+                raise Exception('Unhandled index "{}"'.format(index))
+        had_rejected_result_infos = handle_rejected_result_infos(
+            rejected_result_infos,
+            index_to_name_fn
+        )
+
+        if had_rejected_result_infos:
+            _logger.error('Rejected ResultInfo(s) where found.')
+            return 1
+
+        if len(key_to_result_infos) == 0:
+            _logger.error('No accepeted result infos')
+            return 1
+        had_missing_result_infos = report_missing_result_infos(
+            key_to_result_infos,
+            index_to_name_fn)
+        if had_missing_result_infos:
+            _logger.error('Some result infos were missing')
+            return 1
+
+        # Now do rank
+        keyToRankResultMap = dict()
+        for key, result_info_list in sorted(key_to_result_infos.items(), key=lambda x:x[0]):
+            _logger.info('Ranking "{}"'.format(key))
+            # TODO
+
+    except Exception as e:
+        _logger.error(e)
+        raise e
+
+    return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
