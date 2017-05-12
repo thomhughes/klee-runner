ccb841610708b01dea118e91ec3ccf7112429664
Dan Liew <daniel.liew@imperial.ac.uk>
2017-05-13 00:49:40 +0100

    Teach script to output "all crashes", "all timeouts" and "all crashes or timeouts" sets and their intersections, etc..

--- tools/result-info-fp-bench-coverage.py
+++ tools/result-info-fp-bench-coverage.py
@@ -132,6 +132,16 @@ def main(argv):
         default=None,
         action="store_true"
     )
+    parser.add_argument("--dump-all-crashes-unique",
+        dest="dump_all_crashes_unique",
+        default=None,
+        action="store_true"
+    )
+    parser.add_argument("--dump-all-timeouts-unique",
+        dest="dump_all_timeouts_unique",
+        default=None,
+        action="store_true"
+    )
     parser.add_argument("--categories",
        nargs='+',
        help='Only analyse results where the bencmark belongs to all specified categories',
@@ -206,6 +216,9 @@ def main(argv):
         true_negatives = set() # Stores benchmark names that are expected to not have bugs
         index_to_found_true_negatives = [] # Stores the true negatives found by a tool
         index_to_found_bugs = [] # Maps to a dictionary mapping benchmark names to found errors
+        index_to_all_timeouts = [] # Map index to set of benchmarks where all runs timed out
+        index_to_all_crashes = [] # Map index to set of benchmarks where all runs crashed/OOM.
+        index_to_all_crashes_or_timeouts = [] # Map index to set of benchmarks where all runs crashed/OOM/timedout
         for key, result_info_list in sorted(key_to_result_infos.items(), key=lambda x:x[0]):
             _logger.info('Processing {}'.format(key))
             assert len(result_infos_list) > 1
@@ -217,6 +230,9 @@ def main(argv):
                 for _ in result_info_list:
                     index_to_found_true_negatives.append(set())
                     index_to_found_bugs.append(dict())
+                    index_to_all_timeouts.append(set())
+                    index_to_all_crashes.append(set())
+                    index_to_all_crashes_or_timeouts.append(set())
 
             # Skip benchmarks not in requested categories
             if len(args.categories) > 0:
@@ -271,6 +287,16 @@ def main(argv):
             # Go through tool results and determine how complete each tool's exploration
             # was.
             for index, result_info in enumerate(result_info_list):
+                # Handle crashes and timeouts
+                if result_has_all_crashes(result_info):
+                    _logger.debug('{} has all crashes'.format(key))
+                    index_to_all_crashes[index].add(key)
+                    index_to_all_crashes_or_timeouts[index].add(key)
+                if result_has_all_timeouts(result_info):
+                    _logger.debug('{} has all timeouts'.format(key))
+                    index_to_all_timeouts[index].add(key)
+                    index_to_all_crashes_or_timeouts[index].add(key)
+                # Handle correctness
                 if is_correct_benchmark:
                     if result_info_shows_verified(key, index, result_info):
                         _logger.info('{} reported as correct by {}'.format(
@@ -373,6 +399,44 @@ def main(argv):
                             working_copy_bug_map.pop(other_key, None)
             index_to_found_unique_true_positives.append(working_copy_bug_map)
 
+        # Find benchmarks where only a single tool had timeouts
+        index_to_unique_all_timeouts = [ ]
+        for index, all_timeout_set in enumerate(index_to_all_timeouts):
+            # Start with benchmarks of this tool
+            working_copy_all_timeout_set = all_timeout_set.copy()
+            # Now remove benchmarks they also had all timeouts
+            for other_index, other_all_timeout_set in enumerate(index_to_all_timeouts):
+                if index == other_index:
+                    continue
+                working_copy_all_timeout_set = working_copy_all_timeout_set.difference(
+                    other_all_timeout_set)
+            index_to_unique_all_timeouts.append(working_copy_all_timeout_set)
+        # Find benchmarks where only a single tool had crashes/memouts
+        index_to_unique_all_crashes = [ ]
+        for index, all_crashes_set in enumerate(index_to_all_crashes):
+            # Start with benchmarks of this tool
+            working_copy_all_crashes_set = all_crashes_set.copy()
+            # Now remove benchmarks they also had all crashes
+            for other_index, other_all_crashes_set in enumerate(index_to_all_crashes):
+                if index == other_index:
+                    continue
+                working_copy_all_crashes_set = working_copy_all_crashes_set.difference(
+                    other_all_crashes_set)
+            index_to_unique_all_crashes.append(working_copy_all_crashes_set)
+        # Find benchmarks where only a single tool had crashes/memouts/timeouts
+        index_to_unique_all_crashes_or_timeouts = [ ]
+        for index, all_crashes_or_timeouts_set in enumerate(index_to_all_crashes_or_timeouts):
+            # Start with benchmarks of this tool
+            working_copy_all_crashes_or_timeouts_set = all_crashes_or_timeouts_set.copy()
+            # Now remove benchmarks they also had all crashes
+            for other_index, other_all_crashes_or_timeouts_set in enumerate(index_to_all_crashes_or_timeouts):
+                if index == other_index:
+                    continue
+                working_copy_all_crashes_or_timeouts_set = working_copy_all_crashes_or_timeouts_set.difference(
+                    other_all_crashes_or_timeouts_set)
+            index_to_unique_all_crashes_or_timeouts.append(working_copy_all_crashes_or_timeouts_set)
+
+
 
         # Dump benchmark info
         print("# of benchmark suite expected true negatives: {}".format(len(true_negatives)))
@@ -417,6 +481,48 @@ def main(argv):
                 print("unique:\n{}".format(
                     pprint.pformat(index_to_found_unique_true_positives[index])))
 
+            # All crashes
+            print("  # of all crashes: {} / {}".format(
+                len(index_to_all_crashes[index]),
+                len(key_to_result_infos.keys()))
+            )
+            # All timeouts
+            print("  # of all timeouts: {} / {}".format(
+                len(index_to_all_timeouts[index]),
+                len(key_to_result_infos.keys()))
+            )
+            # All crashes/timoeuts
+            print("  # of all timeouts or crashes: {} / {}".format(
+                len(index_to_all_crashes_or_timeouts[index]),
+                len(key_to_result_infos.keys()))
+            )
+
+            # Only all crashes for this tool
+            print("  # of all crashes only by {}: {} / {}".format(
+                index_to_name_fn(index),
+                len(index_to_unique_all_crashes[index]),
+                len(key_to_result_infos.keys()))
+            )
+            if args.dump_all_crashes_unique:
+                print("uniqiue all crashes:\n{}".format(
+                    pprint.pformat(index_to_unique_all_crashes[index])))
+            # Only all timeouts for this tool
+            print("  # of all timeouts only by {}: {} / {}".format(
+                index_to_name_fn(index),
+                len(index_to_unique_all_timeouts[index]),
+                len(key_to_result_infos.keys()))
+            )
+            if args.dump_all_timeouts_unique:
+                print("unique all timeouts:\n{}".format(
+                    pprint.pformat(index_to_unique_all_timeouts[index])))
+            # Only all timeouts or crashes for this tool
+            print("  # of all timeouts or crashes only by {}: {} / {}".format(
+                index_to_name_fn(index),
+                len(index_to_unique_all_crashes_or_timeouts[index]),
+                len(key_to_result_infos.keys()))
+            )
+
+
 
 
         # Dump correct benchmark intersection info
@@ -452,6 +558,41 @@ def main(argv):
         if args.dump_incorrect_intersection:
             print("Intersection:\n{}".format(pprint.pformat(found_true_positive_intersection)))
 
+        # Dump all crashes intersection info
+        all_crashes_intersection = None
+        for index, all_crashes_set in enumerate(index_to_all_crashes):
+            if all_crashes_intersection is None:
+                all_crashes_intersection = all_crashes_set.copy()
+                continue
+            all_crashes_intersection = all_crashes_intersection.intersection(all_crashes_set)
+        print("# of all crashes intersection: {} / {}".format(
+            len(all_crashes_intersection),
+            len(key_to_result_infos.keys()))
+        )
+        # Dump all timeouts intersection info
+        all_timeouts_intersection = None
+        for index, all_timeouts_set in enumerate(index_to_all_timeouts):
+            if all_timeouts_intersection is None:
+                all_timeouts_intersection = all_timeouts_set.copy()
+                continue
+            all_timeouts_intersection = all_timeouts_intersection.intersection(all_timeouts_set)
+        print("# of all timeouts intersection: {} / {}".format(
+            len(all_timeouts_intersection),
+            len(key_to_result_infos.keys()))
+        )
+        # Dump all timeouts or crashes intersection info
+        all_crashes_or_timeouts_intersection = None
+        for index, all_crashes_or_timeouts_set in enumerate(index_to_all_crashes_or_timeouts):
+            if all_crashes_or_timeouts_intersection is None:
+                all_crashes_or_timeouts_intersection = all_crashes_or_timeouts_set.copy()
+                continue
+            all_crashes_or_timeouts_intersection = all_crashes_or_timeouts_intersection.intersection(all_crashes_or_timeouts_set)
+        print("# of all timeouts or crashes intersection: {} / {}".format(
+            len(all_crashes_or_timeouts_intersection),
+            len(key_to_result_infos.keys()))
+        )
+
+
 
         # Dump complement of correct union info (i.e. what neither tool handled)
         found_true_negative_union = set()
@@ -479,12 +620,78 @@ def main(argv):
         print("# of incorrect in union complement (i.e. not found by any tool): {}".format(
             complement_of_found_true_positive_union_count))
 
+        # Dump complement of "all timeout union"
+        all_timeout_set_union = set()
+        for all_timeout_set in index_to_all_timeouts:
+            all_timeout_set_union = all_timeout_set_union.union(all_timeout_set)
+        all_timeout_set_union_complement = set(key_to_result_infos.keys()).difference(all_timeout_set_union)
+        print("# of all timeouts in union complement (i.e. no tool had all timeouts for these benchmarks): {}".format(
+            len(all_timeout_set_union_complement))
+        )
+
+        # Dump complement of "all crashes union"
+        all_crashes_set_union = set()
+        for all_crashes_set in index_to_all_crashes:
+            all_crashes_set_union = all_crashes_set_union.union(all_crashes_set)
+        all_crashes_set_union_complement = set(key_to_result_infos.keys()).difference(all_crashes_set_union)
+        print("# of all crashes in union complement (i.e. no tool had all crashes for these benchmarks): {}".format(
+            len(all_crashes_set_union_complement))
+        )
+
+
 
     except Exception as e:
         _logger.error(e)
         raise e
 
     return 0
+
+def result_has_all_timeouts(result_info):
+    # Look for hard timeouts
+    num_merged_results = analyse.get_num_merged_results(result_info)
+    outcomes = analyse.get_generic_run_outcomes(result_info)
+    num_hard_timeouts = 0
+    for outcome in outcomes:
+        if outcome.code == analyse.KleeRunnerResult.OUT_OF_TIME:
+            num_hard_timeouts += 1
+    if num_hard_timeouts == num_merged_results:
+        return True
+    # Now try soft timeouts
+    klee_dirs = []
+    if num_merged_results == 1:
+        klee_dirs.append(KleeDir(result_info['klee_dir']))
+    else:
+        for klee_dir_path in result_info['klee_dir']:
+            klee_dirs.append(KleeDir(klee_dir_path))
+
+    klee_dir_outcomes = []
+    for klee_dir in klee_dirs:
+        temp_outcomes = analyse.get_klee_dir_outcomes(
+            klee_dir,
+            check_for_soft_timeout=True)
+        klee_dir_outcomes.extend(temp_outcomes)
+    num_soft_timeouts = 0
+    for outcome in klee_dir_outcomes:
+        if outcome.code == analyse.KleeRunnerResult.OUT_OF_TIME:
+                num_soft_timeouts += 1
+    if num_soft_timeouts == num_merged_results:
+        return True
+    return False
+
+
+def result_has_all_crashes(result_info):
+    num_merged_results = analyse.get_num_merged_results(result_info)
+    outcomes = analyse.get_generic_run_outcomes(result_info)
+    num_crashes = 0
+    num_oom = 0
+    for outcome in outcomes:
+        if outcome.code == analyse.KleeRunnerResult.BAD_EXIT:
+            num_crashes += 1
+        elif outcome.code == analyse.KleeRunnerResult.OUT_OF_MEMORY:
+            num_oom += 1
+    if num_crashes == num_merged_results or num_oom == num_merged_results:
+        return True
+    return False
 
 
 if __name__ == '__main__':
